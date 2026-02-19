ba3b86898db9775c15a1451e64ea844fe044cc00
DARDE Marina <dardemarina@gmail.com>
2026-02-19 21:11:58 +0100

    [SH-168] fix internal user response

--- src/main/java/com/skillshub/gateway_service/user/controller/UserFacadeController.java
+++ src/main/java/com/skillshub/gateway_service/user/controller/UserFacadeController.java
@@ -7,7 +7,6 @@ import com.skillshub.gateway_service.user.service.TutorPromotionService;
 import com.skillshub.gateway_service.user.service.UserProvisioningService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
